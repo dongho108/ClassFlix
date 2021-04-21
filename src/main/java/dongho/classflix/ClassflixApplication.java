4919f29bb218c28e4eadccf83f985f8b167555ba
dongho <dongho108@naver.com>
2021-04-21 14:48:43 +0900

    initial commit

---
+++ src/main/java/dongho/classflix/ClassflixApplication.java
@@ -0,0 +1,13 @@
+package dongho.classflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassflixApplication.class, args);
+	}
+
+}
