f603b3cd079a3f310acb4cd979540f5679bc9084
dongho <dongho108@naver.com>
2021-04-26 10:48:57 +0900

    update Transaction read only

--- src/main/java/dongho/classflix/service/MemberService.java
+++ src/main/java/dongho/classflix/service/MemberService.java
@@ -40,7 +40,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getUserName());
         if (!findMembers.isEmpty()) {
-            throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
         }
     }
 }
