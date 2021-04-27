ea6a5aadfd9a601ca0aedf3dcf7ecd8dcd25e2d7
dongho <dongho108@naver.com>
2021-04-27 15:47:54 +0900

    add home (test data version)

--- src/main/java/dongho/classflix/controller/HomeController.java
+++ src/main/java/dongho/classflix/controller/HomeController.java
@@ -1,10 +1,17 @@
 package dongho.classflix.controller;
 
+import dongho.classflix.service.LectureService;
+import dongho.classflix.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private LectureService lectureService;
 
     @RequestMapping("/")
     public String home() {
