19bd921163b158ec4233b467c032c7f01c97f0e9
dongho <dongho108@naver.com>
2021-04-30 14:03:41 +0900

    add create member form & Controller

--- src/main/java/dongho/classflix/controller/MemberController.java
+++ src/main/java/dongho/classflix/controller/MemberController.java
@@ -1,10 +1,15 @@
 package dongho.classflix.controller;
 
+import dongho.classflix.domain.Member;
 import dongho.classflix.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +21,15 @@ public class MemberController {
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/memberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+        if (result.hasErrors()) {
+            return "members/memberForm";
+        }
+        Member member = new Member(form.getName(), form.getAge(), form.getGender(), form.getCareer());
+        memberService.join(member);
+        return "redirect:/";
     }
 }
