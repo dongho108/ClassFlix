33aa6f14aca09bb1d35169dca3bea58605695065
dongho <dongho108@naver.com>
2021-07-15 14:33:28 +0900

    separate application.yml (main : local, test : test) for only run initDB in main

--- src/main/java/dongho/classflix/InitDB.java
+++ src/main/java/dongho/classflix/InitDB.java
@@ -4,6 +4,7 @@ import dongho.classflix.domain.Gender;
 import dongho.classflix.domain.Lecture;
 import dongho.classflix.domain.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 
+@Profile("local")
 @Component
 @RequiredArgsConstructor
 public class InitDB {
