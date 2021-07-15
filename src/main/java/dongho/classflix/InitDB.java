51e82ca6f1fe51e6f317fcdb13fb44b9ab2b1388
dongho <dongho108@naver.com>
2021-07-15 15:51:12 +0900

    update initDB (add sample data 100)

--- src/main/java/dongho/classflix/InitDB.java
+++ src/main/java/dongho/classflix/InitDB.java
@@ -40,11 +40,15 @@ public class InitDB {
             Lecture lecture2 = new Lecture("스프링코어", "김영한", "나빠요", "클래스101", uri);
             Lecture lecture3 = new Lecture("jpa기초", "김영한", "그냥그래요", "패스트캠퍼스", uri);
             Lecture lecture4 = new Lecture("jpa활용", "김영한", "좋아요", "Class Flix", uri);
-
             em.persist(lecture1);
             em.persist(lecture2);
             em.persist(lecture3);
             em.persist(lecture4);
+
+            for (int i = 0; i < 100; i++) {
+                em.persist(new Lecture("강의"+i, "김영한", "샘플데이터", "사이트"+i, uri));
+            }
+
 
             Member member = new Member("김동호", 25, Gender.FEMALE, "학생");
             em.persist(member);
