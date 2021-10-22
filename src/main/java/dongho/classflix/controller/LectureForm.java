2717f6b70a0f5370098b5f07da09eb9a28827ee9
dongho <dongho108@naver.com>
2021-10-22 21:22:42 +0900

    add LectureForm Validator

--- src/main/java/dongho/classflix/controller/LectureForm.java
+++ src/main/java/dongho/classflix/controller/LectureForm.java
@@ -4,21 +4,25 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import java.net.URI;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class LectureForm {
-    @NotEmpty(message = "강의 제목은 필수 입니다")
+    @NotBlank(message = "강의 제목을 입력해 주세요.")
     private String lectureName;
 
-    @NotEmpty(message = "강의자 이름은 필수 입니다")
+    @NotBlank(message = "강의자 이름을 입력해 주세요.")
     private String teacherName;
 
     private String content;
     private MultipartFile image;
+
+    @NotBlank(message = "강의가 올려져 있는 사이트를 입력해 주세요.")
     private String siteName;
+
+//    @URL(message = "URL을 제대로 입력했는지 확인해 주세요.")
+//    private String uri;
     private URI uri;
 }
